008a37ab281186e1bd7fe048c83e78fae3a027e6
Diman Todorov <diman.todorov@outlook.com>
2022-08-26 15:14:06 -0700

    Add project files.

---
+++ Models/ReflectorModule.cs
@@ -0,0 +1,11 @@
+﻿namespace DStarDash.Models
+{
+    public class ReflectorModule
+    {
+        public string Module { get; set; } = string.Empty;
+        public string Usage { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public string Speed { get; set; } = string.Empty;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DStarDash
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
