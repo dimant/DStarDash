9c6e26e87c6d898979b20e1f893e8d025df67fe3
Diman Todorov <diman.todorov@outlook.com>
2022-08-29 00:36:25 -0700

    special casing html formats

--- Parsers/XlxHtmlParser.cs
+++ Parsers/XlxHtmlParser.cs
@@ -83,7 +83,79 @@
                 return node.ParentNode.ParentNode;
             }
 
-            if (!doc.DocumentNode.InnerHtml.Contains("DSTAR dashboard by David PA7LIM"))
+            node = doc.DocumentNode.SelectSingleNode("//table//tr/th[contains(.,'Nominativo')]");
+
+            if (node != null)
+            {
+                return node.ParentNode.ParentNode;
+            }
+
+            node = doc.DocumentNode.SelectSingleNode("//table//tr/th[contains(.,'Znak')]");
+
+            if (node != null)
+            {
+                return node.ParentNode.ParentNode;
+            }
+
+            node = doc.DocumentNode.SelectSingleNode("//table//tr/th[contains(.,'DV Station')]");
+
+            if (node != null)
+            {
+                return node.ParentNode.ParentNode;
+            }
+
+            node = doc.DocumentNode.SelectSingleNode("//table//tr/th[contains(.,'Çağrı İşareti')]");
+
+            if (node != null)
+            {
+                return node.ParentNode.ParentNode;
+            }
+
+            node = doc.DocumentNode.SelectSingleNode("//table//tr/th[contains(.,'Rufzeichen')]");
+
+            if (node != null)
+            {
+                return node.ParentNode.ParentNode;
+            }
+
+            node = doc.DocumentNode.SelectSingleNode("//table//tr/th[contains(.,'Инициал')]");
+
+            if (node != null)
+            {
+                return node.ParentNode.ParentNode;
+            }
+
+            node = doc.DocumentNode.SelectSingleNode("//table//th[contains(.,'MyCall')]");
+
+            if (node != null)
+            {
+                return node.ParentNode;
+            }
+
+            if (!doc.DocumentNode.InnerHtml.Contains("DSTAR dashboard by David PA7LIM")
+                && !doc.DocumentNode.InnerHtml.Contains("http://93.186.254.219/db/index.php")
+                && !doc.ParsedText.Contains("EASYDNS-FORWARDER")
+                && !doc.ParsedText.Contains("http://bh4jbv.site/xlxd/")
+                && !doc.ParsedText.Contains("http://xlx.xrf105.fr")
+                && !doc.ParsedText.Contains("http://80.211.94.145/214/")
+                && !doc.ParsedText.Contains("http://162.238.214.18/xlx256/")
+                && !doc.ParsedText.Contains("http://178.254.33.188/")
+                && !doc.ParsedText.Contains("http://179.51.0.153:8503/dashboard/index.php")
+                && !doc.ParsedText.Contains("https://ke0tcf.radio/Utilities/Reflector")
+                && !doc.ParsedText.Contains("http://dv.pd3rfr.nl:8080/xlxd/")
+                && !doc.ParsedText.Contains("http://45.32.82.218/db/")
+                && !doc.ParsedText.Contains("http://xlx991.iz7auh.net/index.php")
+                && !doc.ParsedText.Contains("XLX246 Reflector Dashboard")
+                && !doc.ParsedText.Contains("je4smq@jarl.com")
+                && !doc.ParsedText.Contains("<meta http-equiv=\"refresh\" content=")
+                && !doc.ParsedText.Contains("<meta http-equiv='Refresh'content =")
+                && !doc.ParsedText.Contains("LX1IQ and Dashboard modifed by HB9GFX")
+                && !doc.ParsedText.Contains("Reflecteur et Serveur Starnet multi protocoles francophone, fourni par F5KAV")
+                && !doc.ParsedText.Contains("If you see this page, the nginx web server is successfully installed and")
+                && !doc.ParsedText.Contains("This is the default welcome page used to test the correct")
+                && !doc.ParsedText.Contains("Welcome to CentOS")
+                && !doc.ParsedText.Contains("The requested URL was not found on this server.")
+                && !doc.ParsedText.Contains("You don't have permission to access this resource."))
             {
                 throw new Exception("Couldn't find heard users table.");
             }
@@ -99,23 +171,46 @@
             for(int index = 0; index < thNodes.Count(); index++)
             {
                 var name = thNodes[index].InnerText;
-                if (name == "Callsign" || name == "MyCall")
+                if (name == "Callsign" 
+                    || name == "MyCall"
+                    || name == "Nominativo"
+                    || name == "Znak"
+                    || name == "DV Station"
+                    || name == "Çağrı İşareti"
+                    || name == "Rufzeichen"
+                    || name == "Инициал")
                 {
                     result[ColumnType.Callsign] = new ColumnAssociation(name, index);
                 }
-                else if (name == "Last heard" || name == "Last heard (UTC)")
+                else if (name == "Last heard"
+                    || name == "Last heard (UTC)"
+                    || name == "Last Heard - UTC"
+                    || name == "Last heard EST/EDT"
+                    || name == "Last heard (JST)"
+                    || name == "Last heard (GMT)"
+                    || name == "Last heard (Local)"
+                    || name == "Last Heard"
+                    || name == "Last TX"
+                    || name == "Ascoltato"
+                    || name == "Godzina"
+                    || name == "Son Duyulma"
+                    || name == "Zuletzt gehoert"
+                    || name == "Последно чут")
                 {
                     result[ColumnType.LastHeard] = new ColumnAssociation(name, index);
                 }
                 else if (
                     thNodes[index].ChildNodes.FirstOrDefault()?.GetAttributeValue("alt", string.Empty) == "Listening on"
-                    || name == "Module")
+                    || name == "Module"
+                    || name == "Group"
+                    || name == "Modulo")
                 {
                     result[ColumnType.HeardOn] = new ColumnAssociation("Listening on", index);
                 }
             }
 
-            if (result.Count() != 3)
+            if (!(result.Count() == 3
+                ||(result.Count() == 2 && result.ContainsKey(ColumnType.HeardOn) == false)))
             {
                 throw new Exception("Couldn't find all columns.");
             }
@@ -143,7 +238,63 @@
 
             try
             {
+                return DateTime.ParseExact(date, "MM-dd-yyyy HH:mm", CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+            }
+
+            try
+            {
                 return DateTime.ParseExact(date, "yyyy.MM.dd HH:mm", CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+            }
+
+            try
+            {
+                return DateTime.ParseExact(date, "yyyy.MM.dd. HH:mm:ss", CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+            }
+
+            try
+            {
+                return DateTime.ParseExact(date, "yyyy.MM.dd HH:mm:ss", CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+            }
+
+            try
+            {
+                return DateTime.ParseExact(date, "dd.MM.yyyy - HH:mm", CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+            }
+
+            try
+            {
+                return DateTime.ParseExact(date, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+            }
+
+            try
+            {
+                return DateTime.ParseExact(date, "MM/dd/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+            }
+
+            try
+            {
+                return DateTime.ParseExact(date, "HH:mm", CultureInfo.InvariantCulture);
             }
             catch
             {
